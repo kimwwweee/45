18ebf2b12ea5f2246f0dc88254d6c5384b8fc1fd
kimwwweee <cheonghak.t230206@ggh.goe.go.kr>
2025-06-17 13:21:21 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,786 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2--2",
+      "name": "2층 중앙 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10604882635736956,
+          "pitch": 0.28959498087586155,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 2.02258112048676,
+          "pitch": 0.31514661678984623,
+          "rotation": 0,
+          "target": "2-2-"
+        },
+        {
+          "yaw": -1.6942725720856266,
+          "pitch": 0.5406861438920103,
+          "rotation": 0,
+          "target": "1-2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.461595885473333,
+          "pitch": -0.004868750711359837,
+          "title": "서버실",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.9518664703782314,
+          "pitch": 0.013553262431443613,
+          "title": "교육정보부",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.0373695078826657,
+          "pitch": -0.021079764952945723,
+          "title": "보건실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-2-",
+      "name": "2층 중앙",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7446819529023134,
+          "pitch": 0.3380308725687282,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.4661450630474171,
+          "pitch": 0.18381926039909757,
+          "rotation": 0,
+          "target": "0-2--2"
+        },
+        {
+          "yaw": -0.9330297927170932,
+          "pitch": 0.11939041383006632,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.1362151528723983,
+          "pitch": -0.05978738815962714,
+          "title": "교육정보부",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.9598245530272713,
+          "pitch": -0.12281633992292562,
+          "title": "방송실",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.0051166745678444,
+          "pitch": 0.11456271542322582,
+          "title": "서버실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-2-",
+      "name": "2층 학안",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7606470045462643,
+          "pitch": 0.32224698155034304,
+          "rotation": 0,
+          "target": "12-6"
+        },
+        {
+          "yaw": -2.499295648064848,
+          "pitch": 0.4946707203015013,
+          "rotation": 0,
+          "target": "0-2--2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.894695874140993,
+          "pitch": -0.08955816445589093,
+          "title": "학생안전부(?)",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "위클래스",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.86515258771985,
+          "pitch": 0.2626455667066736,
+          "rotation": 0,
+          "target": "1-2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.682142037535538,
+          "pitch": -0.056826793026118594,
+          "title": "본교무실",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.4636379498851646,
+          "pitch": 0.04681199501194122,
+          "title": "위클래스",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "송천홀",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9354354244710121,
+          "pitch": 0.3795421859830501,
+          "rotation": 0,
+          "target": "1-2-"
+        },
+        {
+          "yaw": -2.7590119762287717,
+          "pitch": 0.5651021960394846,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.287238827486327,
+          "pitch": 0.00655628387195506,
+          "title": "튼튼 송천홀",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-2----",
+      "name": "2층 과학실 찍고 바로 삭제예정",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4849786680546604,
+          "pitch": 0.6686523008910967,
+          "rotation": 0,
+          "target": "7--2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.657544090054042,
+          "pitch": -0.11536699406267559,
+          "title": "학부모실",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.91344440968375,
+          "pitch": -0.087974154504316,
+          "title": "융합과학부",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6--",
+      "name": "과학실 복도",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0907747137902533,
+          "pitch": 0.5459515667218309,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.907425453126323,
+          "pitch": 0.5528746237719346,
+          "rotation": 0,
+          "target": "7--2"
+        },
+        {
+          "yaw": 0.4831121349325276,
+          "pitch": 0.37967713410008486,
+          "rotation": 0,
+          "target": "11-"
+        },
+        {
+          "yaw": -2.6408121874545945,
+          "pitch": 0.3666821728770735,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.49021491529734895,
+          "pitch": 0.19908786120771893,
+          "title": "화학실",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.6370989436064285,
+          "pitch": 0.19761381251539234,
+          "title": "물리지학실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7--2",
+      "name": "과학실 복도2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2011715731800265,
+          "pitch": 0.6108485362861575,
+          "rotation": 0,
+          "target": "6--"
+        },
+        {
+          "yaw": 1.9519097235572804,
+          "pitch": 0.40853333045709306,
+          "rotation": 0,
+          "target": "5-2----"
+        },
+        {
+          "yaw": -2.8485852922068524,
+          "pitch": 0.3906106822744384,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 0.5356828211970619,
+          "pitch": 0.4141168445580199,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": -1.6606564252510907,
+          "pitch": 0.13128457760659096,
+          "rotation": 0,
+          "target": "10-"
+        },
+        {
+          "yaw": -0.6378089042270503,
+          "pitch": 0.1642458527905042,
+          "rotation": 0,
+          "target": "11-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8180595048777075,
+          "pitch": 0.2114119199499207,
+          "title": "생명과학실",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.510693964064675,
+          "pitch": 0.208597462211646,
+          "title": "세미나실",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.6378436816050161,
+          "pitch": 0.024079603082059364,
+          "title": "물리지학실",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6366920030878127,
+          "pitch": -0.04973575935581209,
+          "title": "화학실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "생명",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6854230295869854,
+          "pitch": 0.011973889774914426,
+          "rotation": 0,
+          "target": "7--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "세미나실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0815178776429644,
+          "pitch": 0.05804530019661769,
+          "rotation": 0,
+          "target": "7--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "물리",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24553346393998865,
+          "pitch": 0.021979986233809257,
+          "rotation": 0,
+          "target": "7--2"
+        },
+        {
+          "yaw": -2.1649833476344185,
+          "pitch": 0.007613805916662386,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-",
+      "name": "화학",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1372893355663454,
+          "pitch": 0.02227251425014387,
+          "rotation": 0,
+          "target": "7--2"
+        },
+        {
+          "yaw": -1.1445398294982017,
+          "pitch": 0.03405892330508209,
+          "rotation": 0,
+          "target": "6--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-6",
+      "name": "복도6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4763236777606537,
+          "pitch": 0.331540741142792,
+          "rotation": 0,
+          "target": "13-"
+        },
+        {
+          "yaw": 0.4417399639593178,
+          "pitch": 0.29444860393355654,
+          "rotation": 0,
+          "target": "2-2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.469217205458686,
+          "pitch": 0.12830377036017282,
+          "title": "체력단련실",
+          "text": "Text"
+        },
+        {
+          "yaw": -3.1384076106190086,
+          "pitch": 0.054665924354551976,
+          "title": "교직원 휴게소(여)",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.9194360991353143,
+          "pitch": 0.018040299297354068,
+          "title": "남직원휴게소",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.8528869962225016,
+          "pitch": -0.02120032128759064,
+          "title": "학생회실",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "13-",
+      "name": "체단실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2967832773079468,
+          "pitch": 0.0596749743428191,
+          "rotation": 0,
+          "target": "12-6"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
